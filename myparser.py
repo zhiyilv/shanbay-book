5df4fa17d1ee7279d11bbc3106a8c3e2de461631
zhiyilv <zhiyilv@outlook.com>
2017-02-23 17:58:06 +0800

    添加lemmatizer 使用nltk中WordNet的Lemmatizer获取单词的基本形式

--- myparser.py
+++ myparser.py
@@ -1,6 +1,7 @@
 import string
 import enchant
-from enchant.tokenize import get_tokenizer
+# from enchant.tokenize import get_tokenizer
+from nltk.stem.wordnet import WordNetLemmatizer
 
 
 def get_words_from_ass(path):
@@ -9,7 +10,7 @@ def get_words_from_ass(path):
         for line in f:
             if line.startswith('Dialogue'):
                 seg = line[line.rfind('}')+1:len(line)-1].lower()
-                seg = ''.join([i for i in seg if i==' ' or i in string.ascii_lowercase])
+                seg = ''.join([i for i in seg if i == ' ' or i in string.ascii_lowercase])
                 pieces = seg.split(' ')
                 for piece in pieces:
                     if piece not in extracted:
@@ -20,7 +21,8 @@ def get_words_from_ass(path):
 # using enchant to exclude non-words
 def get_words_from_ass_2(path):
     with open(path, 'r', encoding='utf-16-le') as f:
-        d = enchant.Dict('en_US')
+        d = enchant.Dict('en_US')  # to check words
+        lmtzr = WordNetLemmatizer()  # transform words to the basic form
         extracted = set()
         notword = set()
         for line in f:
@@ -29,10 +31,16 @@ def get_words_from_ass_2(path):
                 seg = ''.join([i for i in seg if i == ' ' or i in string.ascii_lowercase])
                 pieces = seg.split(' ')
                 for piece in pieces:
-                    if d.check(piece):
-                        extracted.add(piece)
-                    else:
-                        notword.add(piece)
+                    if piece != '':
+                        word1 = lmtzr.lemmatize(piece)  # for noun.
+                        if piece != word1:
+                            w = word1
+                        else:
+                            w = lmtzr.lemmatize(piece, 'v')  # probably not a noun.
+                        if d.check(w):
+                            extracted.add(w)
+                        else:
+                            notword.add(w)
     return extracted, notword
 
 
