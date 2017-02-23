455ec015019cbacad0a8f561a5e9be2f9b585235
zhiyilv <zhiyilv@outlook.com>
2017-02-23 15:36:23 +0800

    添加单词检测 使用pyenchant检测是否为英文单词

--- myparser.py
+++ myparser.py
@@ -17,20 +17,25 @@ def get_words_from_ass(path):
     return extracted
 
 
+# using enchant to exclude non-words
 def get_words_from_ass_2(path):
     with open(path, 'r', encoding='utf-16-le') as f:
-        extracted = []
+        d = enchant.Dict('en_US')
+        extracted = set()
+        notword = set()
         for line in f:
             if line.startswith('Dialogue'):
                 seg = line[line.rfind('}') + 1:len(line) - 1].lower()
                 seg = ''.join([i for i in seg if i == ' ' or i in string.ascii_lowercase])
                 pieces = seg.split(' ')
                 for piece in pieces:
-                    if piece not in extracted:
-                        extracted.append(piece)
-    return extracted
+                    if d.check(piece):
+                        extracted.add(piece)
+                    else:
+                        notword.add(piece)
+    return extracted, notword
 
 
-def split_words(str, t=None):
-    if not t:
-        t = get_tokenizer('en_US')
+# def split_words(str, t=None):
+#     if not t:
+#         t = get_tokenizer('en_US')
