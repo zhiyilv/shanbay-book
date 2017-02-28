9c1f62ec1c00cdd0ab8d2a191e0810e128a52e7f
zhiyilv <zhiyilv@outlook.com>
2017-02-28 16:04:54 +0800

    修正season 之前是list，应该是一个string

--- imdb.py
+++ imdb.py
@@ -4,11 +4,10 @@ from bs4 import BeautifulSoup as bs
 from os import path, makedirs
 
 
-
 # this is for imdb.com
 def get_titles_synopsis(url):
     t = requests.get(url).text
-    season = re.findall(r'&nbsp;<strong>Season (\d)<', t)
+    season = re.findall(r'&nbsp;<strong>Season (\d)<', t)[0]
     titles = re.findall(r'<strong><a href="/title/.*\n?title="(.*?)"', t)
     synopsis = re.findall(r'itemprop="description">\n*(.*?)\s*</div', t, re.DOTALL)
     synopsis = [re.sub(r'<.*?>', '', i) for i in synopsis]  # delete labels in synopsis
@@ -17,7 +16,7 @@ def get_titles_synopsis(url):
     return season, titles, synopsis
 
 
-#this is for douban.com
+# this is for douban.com
 def get_poster(url, store_path='.\\'):
     """
     input the url of the movie on douban.com
@@ -26,18 +25,22 @@ def get_poster(url, store_path='.\\'):
     :param store_path:
     :return:
     """
+    print('Getting poster url...')
     movie_page = requests.get(url).content
     posters_url = bs(movie_page, 'lxml').find('a', class_='nbgnbg')['href']
     posters_page = requests.get(posters_url).content
     poster_url = bs(posters_page, 'lxml').find('div', class_='cover').find('a')['href']
     poster_page = requests.get(poster_url).content
     pic_url = bs(poster_page, 'lxml').find('a', class_='mainphoto').find('img')['src']
+    print('It is at {}'.format(pic_url))
     res = requests.get(pic_url)
 
     if not path.exists(store_path):
         makedirs(store_path)
     with open(path.join(store_path, 'poster.jpg'), 'wb') as f:
         f.write(res.content)
+
+    print('The poster is stored at {}'.format(store_path))
 
 
 
